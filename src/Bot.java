24037fa4ac3eaa42dfd463899b6c86f710c0dcda
nikita <audi.087@yandex.ru>
2018-01-26 15:23:48 +0400

    Create test model bot

---
+++ src/Bot.java
@@ -0,0 +1,69 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot
+{
+
+    public static void main(String[] args)
+    {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+        try
+        {
+            telegramBotsApi.registerBot(new Bot());
+        }
+        catch (TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private void sendMessange(Message message, String text)
+    {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText(text);
+
+        try
+        {
+            sendMessage(sendMessage);
+        }
+        catch (TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update)
+    {
+        Message message = update.getMessage();
+
+        String text = message.getText();
+
+        if(text.equals("Привет"))
+        {
+            sendMessange(message,"Здравствуй!");
+        }
+    }
+
+    @Override
+    public String getBotUsername()
+    {
+        return Config.NAME_BOT;
+    }
+
+    @Override
+    public String getBotToken()
+    {
+        return Config.TOKEN_BOT;
+    }
+}
---
+++ src/Config.java
@@ -0,0 +1,5 @@
+public class Config
+{
+    public static final String TOKEN_BOT = "473443441:AAFmfkBO96qLHUNse5Sx2EDX0TSx2AWv3Hg";
+    public static final String NAME_BOT = "Kathleen_bot";
+}
